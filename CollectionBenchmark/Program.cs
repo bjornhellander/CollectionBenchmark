462fd7bc3306a5c05e9409ff65399550e358647c
Björn Hellander <bjorn.hellander@gmail.com>
2022-11-12 14:43:40 +0100

    Add project files.

---
+++ CollectionBenchmark/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionBenchmark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
